2fb4d76426b4dc7ce9fdc73436ea10bd4a1729bf
mshogren <3902009+mshogren@users.noreply.github.com>
2017-01-11 15:20:12 -0700

    Fixing a listener leak

--- server.js
+++ server.js
@@ -10,20 +10,28 @@ process.on('SIGTERM', gracefulShutdown);
 process.on('SIGINT', gracefulShutdown);
 
 backend.on('login', (db) => {
-  bbq.on('temperatureChange', (data) => {
-    db.addState(data);
-  });
+  if (bbq.listenerCount('temperatureChange') === 0) {
+    bbq.on('temperatureChange', (data) => {
+      db.addState(data);
+    });
+  }
 
-  db.on('setTargetTemperature', (temperature) => {
-    bbq.setTarget(temperature);
-  });
+  if (db.listenerCount('setTargetTemperature') === 0) {
+    db.on('setTargetTemperature', (temperature) => {
+      bbq.setTarget(temperature);
+    });
+  }
 
-  db.on('addSensor', (sensorData) => {
-    bbq.addSensor(sensorData);
-  });
+  if (db.listenerCount('addSensor') === 0) {
+    db.on('addSensor', (sensorData) => {
+      bbq.addSensor(sensorData);
+    });
+  }
 
-  db.on('removeSensor', (sensorData) => {
-    bbq.removeSensor(sensorData);
-  });
+  if (db.listenerCount('removeSensor') === 0) {
+    db.on('removeSensor', (sensorData) => {
+      bbq.removeSensor(sensorData);
+    });
+  }
 });
 
