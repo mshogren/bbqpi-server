eab3ab556adebffc1482d307188cbf5924bdf81a
mshogren <3902009+mshogren@users.noreply.github.com>
2017-11-03 10:55:05 -0600

    test(config): Start fixing config

--- src/config/config.test.js
+++ src/config/config.test.js
@@ -3,7 +3,7 @@ const config = require('./config');
 describe('The config object', () => {
   test('is named config and stores objects prettily in a single file', () => {
     expect(config).toMatchObject({
-      name: 'config',
+      name: 'config/config',
       _single: true,
       _pretty: true,
     });
