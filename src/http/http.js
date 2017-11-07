89f71ed252da448f355dfb7f19c08f1b5f4f5510
mshogren <3902009+mshogren@users.noreply.github.com>
2017-11-07 15:29:21 -0700

    feat(http): Get resin-supervisor status

--- src/http/http.js
+++ src/http/http.js
@@ -1,16 +1,21 @@
 const http = require('http');
+const request = require('request');
 const bonjour = require('bonjour')();
 
 function HttpServer() {
   if (!(this instanceof HttpServer)) return new HttpServer();
 
   const port = 80;
+  const resinSupervisorUrl = `${process.env.RESIN_SUPERVISOR_ADDRESS}/v1/device?apikey=${process.env.RESIN_SUPERVISOR_API_KEY}`;
 
   this.deviceStatus = {};
 
-  http.createServer((request, response) => {
-    response.setHeader('Content-Type', 'application/json');
-    response.end(JSON.stringify(this.deviceStatus));
+  http.createServer((req, res) => {
+    request.get(resinSupervisorUrl, (err, resinResponse, body) => {
+      console.log(body);
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(this.deviceStatus));
+    });
   }).listen(port);
 
   bonjour.publish({ name: 'BBQ-Pi', type: 'http', port: 80 });
